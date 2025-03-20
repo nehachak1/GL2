9c9f83b290e271b01ddd6a3bf6cec64ba4075042
nehachak1 <neha.chakraborty@epfl.ch>
2025-03-20 17:48:16 +0100

    Done with 2.3 Gouraud Shading

--- src/shade_pervertex.vert.glsl
+++ src/shade_pervertex.vert.glsl
@@ -7,7 +7,7 @@ attribute vec3 vertex_normal;
 /* #TODO GL2.3
 	Pass the values needed for per-pixel illumination by creating a varying vertex-to-fragment variable.
 */
-//varying ...
+varying vec3 frag_color;
 
 // Global variables specified in "uniforms" entry of the pipeline
 uniform mat4 mat_mvp;
@@ -28,5 +28,35 @@ void main() {
 
 	Hint: Compute the vertex position, normal and light_position in view space. 
 	*/
+
+	// Vertex_position is in object space - transformed to view space 
+	// Lighting calculations are done in view space 
+	// Position view - direction of vertex to camera
+    vec3 position_view = (mat_model_view * vec4(vertex_position, 1.0)).xyz;
+											// homogenous vector with w = 1.0
+
+    // Transform normal (in object space) to view space
+    vec3 normal_view = normalize(mat_normals_to_view * vertex_normal);
+
+    // Compute light direction which is in view space
+    vec3 light_direction = normalize(light_position - position_view);
+
+    // Opposite of position_view
+    vec3 view_direction = normalize(-position_view);
+
+    // Compute lighting like with its 3 factors (ambient, diffuse, specular) 
+    vec3 half_vector = normalize(light_direction + view_direction);
+
+    vec3 ambient = material_ambient * light_color * material_color;
+
+    float diffuse_contribution = max(dot(normal_view, light_direction), 0.0);
+    vec3 diffuse = material_color * light_color * diffuse_contribution;
+
+    float specular_factor = max(dot(normal_view, half_vector), 0.0);
+    vec3 specular = material_color * light_color * pow(specular_factor, material_shininess);
+
+    frag_color = ambient + diffuse + specular;
 	gl_Position = mat_mvp * vec4(vertex_position, 1);
 }
+
+	
